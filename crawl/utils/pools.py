ef9ffabd88587c27c4dbacd969457cc09c5418f2
bosslsk <lushangkun1228@hotmail.com>
2018-03-22 14:23:53 +0800

    工具包: redis和mongo数据库连接池

---
+++ crawl/utils/pools.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+"""
+    @author: harvey
+    @time: 2018/3/22 0:23
+    @subject: 
+"""
+
+import pymongo
+import redis
+
+from crawl.settings import config
+
+
+_client = pymongo.MongoClient(config.MONGO_URI)
+mongo_db = _client[config.MONGO_DB_NAME]
+_auth = config.MONGO_AUTH
+
+if _auth:
+    mongo_db.authenticate(**_auth)
+
+redis_connection_pool = redis.ConnectionPool.from_url(config.REDIS_URI)
