7dd5bfe32a25c7758d2fefa6dc51b566796ede85
bosslsk <lushangkun1228@hotmail.com>
2018-03-26 18:54:26 +0800

    晋江request_data构造

--- crawl/run/prepare_data.py
+++ crawl/run/prepare_data.py
@@ -33,13 +33,27 @@ def collect_data(spider_name, mongodb, limit=0):
         '_id': 0, 'source_id': 1, 'book_id': 1, 'tar_category': 1, 'sub_category': 1,
         'title': 1, 'url': 1, 'relate_id': 1, 'author': 1
     }
-    data = []
     if 'source' in data_coll:
         start_url_info = config.SOURCE_START_URL_DICT[spider_name.split('_')[0]]
-        for tar in start_url_info['tag'].keys():
-            tar_category = tar
-            for sub in start_url_info['tag'][tar]:
-                source_category = sub.split('#')[0]
+        data = source_data(source_id, start_url_info)
+    else:
+        data = list(mongodb['material_source'].find({'source_id': source_id}, param).limit(limit))
+    return data
+
+
+def source_data(source_id, start_url_info):
+    """
+    不同站点request_data的构造
+    :param source_id:
+    :param start_url_info:
+    :return:
+    """
+    data = []
+    for tar in start_url_info['tag'].keys():
+        tar_category = tar
+        for sub in start_url_info['tag'][tar]:
+            source_category = sub.split('#')[0]
+            if source_id == 1:
                 chanId = sub.split('#')[1].split('_')[0]
                 subCateId = sub.split('#')[1].split('_')[1]
                 data_info = {
@@ -50,8 +64,16 @@ def collect_data(spider_name, mongodb, limit=0):
 
                 }
                 data.append(data_info)
-    else:
-        data = list(mongodb['material_source'].find({'source_id': source_id}, param).limit(limit))
+            if source_id == 7:
+                bq = sub.split('#')[1]
+                data_info = {
+                    'data_url': start_url_info['url'].format(bq=bq),
+                    'source_id': source_id,
+                    'tar_category': tar_category,
+                    'source_category': source_category
+
+                }
+                data.append(data_info)
     return data
 
 
