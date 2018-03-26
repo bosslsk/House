c5d247956648255192a6c0cd0c1e7fa99da33995
bosslsk <lushangkun1228@hotmail.com>
2018-03-26 15:05:01 +0800

    request_data: 准备请求的数据

---
+++ crawl/run/prepare_data.py
@@ -0,0 +1,70 @@
+# -*- coding:utf-8 -*-
+"""
+    @author: harvey
+    @time: 2018/3/5 12:01
+    @subject: 
+"""
+
+from crawl.settings import config
+
+
+def generate_id_coll(spider_name):
+    """
+    获取站点和数据库表名
+    :param spider_name:
+    :return:
+    """
+    source, type_ = spider_name.split('_')
+    source_id = config.SOURCE_DICT[source]
+    data_coll = config.MATERIAL_DICT[type_]
+    return source_id, data_coll
+
+
+def collect_data(spider_name, mongodb, limit=0):
+    """
+    构造数据
+    :param spider_name:
+    :param mongodb: 数据库连接池
+    :param limit:
+    :return:
+    """
+    source_id, data_coll = generate_id_coll(spider_name)
+    param = {
+        '_id': 0, 'source_id': 1, 'book_id': 1, 'tar_category': 1, 'sub_category': 1,
+        'title': 1, 'url': 1, 'relate_id': 1, 'author': 1
+    }
+    data = []
+    if 'source' in data_coll:
+        start_url_info = config.SOURCE_START_URL_DICT[spider_name.split('_')[0]]
+        for tar in start_url_info['tag'].keys():
+            tar_category = tar
+            for sub in start_url_info['tag'][tar]:
+                source_category = sub.split('#')[0]
+                chanId = sub.split('#')[1].split('_')[0]
+                subCateId = sub.split('#')[1].split('_')[1]
+                data_info = {
+                    'data_url': start_url_info['url'].format(chanId=chanId, subCateId=subCateId),
+                    'source_id': source_id,
+                    'tar_category': tar_category,
+                    'source_category': source_category
+
+                }
+                data.append(data_info)
+    else:
+        data = list(mongodb['material_source'].find({'source_id': source_id}, param).limit(limit))
+    return data
+
+
+def generate_data(spider_name, mongodb, limit=0):
+    """
+    request_data请求数据
+    :param spider_name:
+    :param mongodb:
+    :param limit:
+    :return:
+    """
+    source_id, data_coll = generate_id_coll(spider_name)
+    data = collect_data(spider_name, mongodb, limit)
+    if 'source' in data_coll:
+        return data
+    return data
